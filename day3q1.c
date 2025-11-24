62efb28bf5652ec08816b130bf1ed94abb2d64b7
Tejas451807 <tejasy000@gmail.com>
2025-11-25 00:14:19 +0530

    Add files via upload

---
+++ day3q1.c
@@ -0,0 +1,26 @@
+  //  Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/ //
+#include <stdio.h>
+int main() {
+    // celcius = c //
+    float c = 0.0;
+    printf("The Tempreature from celcius to fahrenhiet is %f\n" ,  9.0/5.0*c + 32.0);
+     // celcius = x //
+    float x = 100.0;
+    printf("The Tempreature from celcius to fahrenhiet is %f\n" , 9.0/5.0*x + 32.0);
+
+return 0;
+}
