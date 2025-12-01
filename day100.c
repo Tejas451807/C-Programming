5691822c221c6c040e5580bda232008d9f11d057
Tejas451807 <tejasy000@gmail.com>
2025-12-01 22:16:09 +0530

    Add files via upload

---
+++ day100.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    int marks;
+};
+
+int main() {
+    struct Student *ptr = (struct Student*)malloc(sizeof(struct Student));
+    
+    scanf("%s %d %d", ptr->name, &ptr->roll, &ptr->marks);
+    
+    printf("Modified Data: Name: %s | Roll: %d | Marks: %d\n", ptr->name, ptr->roll, ptr->marks);
+    
+    free(ptr);
+    return 0;
+}
